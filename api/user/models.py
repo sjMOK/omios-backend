46b70de232136e0b91494852228f8dc72eec19c6
sjMOK <mokseungju@naver.com>
2021-11-22 19:38:05 +0900

    User모델 삭제기능 관련 is_active 필드 추가

--- api/user/models.py
+++ api/user/models.py
@@ -33,6 +33,7 @@ class User(AbstractBaseUser):
     is_admin = models.BooleanField(default=False)
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=20)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         db_table = 'user'
