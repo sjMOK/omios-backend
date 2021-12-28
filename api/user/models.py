11027958c5f05a7ae6fd4f0a8001145ad8b50f28
sjMOK <mokseungju@naver.com>
2021-12-28 19:33:46 +0900

    user model 상속 구조 변경, serializer 변경

--- api/user/models.py
+++ api/user/models.py
@@ -7,7 +7,6 @@ class UserManager(BaseUserManager):
     def create_user(self, username, password, **extra_fields):
         user = self.model(
             username=username,
-            **extra_fields
         )
         
         user.set_password(password)
@@ -36,12 +35,11 @@ class User(AbstractBaseUser):
     id = BigAutoField(primary_key=True)
     username = CharField(max_length=20, unique=True)
     is_admin = BooleanField(default=False)
-    email = EmailField(max_length=50)
-    phone = CharField(max_length=15)
     is_active = BooleanField(default=True)
     last_update_password = DateTimeField(default=timezone.now)
 
     class Meta:
+        managed = False
         db_table = 'user'
 
     objects = UserManager()
@@ -49,15 +47,17 @@ class User(AbstractBaseUser):
     USERNAME_FIELD = 'username'
 
 
-class Shopper(Model):
+class Shopper(User):
     name = CharField(max_length=20)
     nickname = CharField(max_length=20, unique=True)
+    email = EmailField(max_length=50)
+    phone = CharField(max_length=15, unique=True)
     gender = BooleanField()
     birthday = DateField()
     height = IntegerField(null=True)
     weight = IntegerField(null=True)
     membership = ForeignKey(Membership, DO_NOTHING, default=1)
-    user = OneToOneField('User', CASCADE, primary_key=True)
+    user = OneToOneField(User, DO_NOTHING, parent_link=True, primary_key=True)
 
     class Meta:
         managed = False
@@ -67,9 +67,12 @@ class Shopper(Model):
         return self.name
 
 
-class Wholesaler(Model):
-    user = OneToOneField(User, CASCADE, primary_key=True)
+class Wholesaler(User):
+    user = OneToOneField(User, DO_NOTHING, parent_link=True, primary_key=True)
     name = CharField(max_length=60)
+    email = EmailField(max_length=50)
+    phone = CharField(max_length=15)
+    company_registration_number = CharField(max_length=12, unique=True)
 
     class Meta:
         managed = False
--- api/user/serializers.py
+++ api/user/serializers.py
@@ -1,5 +1,6 @@
 from random import randint
 from django.utils import timezone
+from django.contrib.auth.hashers import make_password
 from rest_framework.serializers import *
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer, TokenBlacklistSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -7,6 +8,7 @@ from rest_framework_simplejwt.utils import datetime_from_epoch
 from rest_framework.validators import UniqueValidator
 
 from . import models, validators
+
 
 class UserAccessTokenSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -47,28 +49,13 @@ class MembershipSerializer(ModelSerializer):
         fields = '__all__'
 
 
-class UserSerializer(ModelSerializer):
-    username = RegexField(r'^[a-zA-Z0-9]+$', min_length=4, max_length=30, validators=[UniqueValidator(queryset=models.User.objects.all())])
-    password = RegexField(r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[!-~]+$', max_length=128, min_length=10)
-    phone = RegexField(r'^01[0|1|6|7|8|9][0-9]{7,8}$')
-
-    class Meta:
-        model = models.User
-        fields = '__all__'
-
-    def validate(self, data):
-        if 'password' in data.keys():
-            validators.PasswordSimilarityValidator().validate(data['password'], data['username'], data['email'])
-
-        return data
-
-    
 class ShopperSerializer(ModelSerializer):
-    user = UserSerializer()
+    username = RegexField(r'^[a-zA-Z0-9]+$', min_length=4, max_length=20, validators=[UniqueValidator(queryset=models.User.objects.all())])
+    password = RegexField(r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[!-~]+$', max_length=128, min_length=10)
+    phone = RegexField(r'^01[0|1|6|7|8|9][0-9]{7,8}$', validators=[UniqueValidator(queryset=models.Shopper.objects.all())])
     name = RegexField(r'^[가-힣]+$', max_length=20)
     nickname = RegexField(r'^[a-z0-9._]+$', min_length=4, max_length=20, required=False, validators=[UniqueValidator(queryset=models.Shopper.objects.all())])
-    membership = MembershipSerializer(required=False)
-
+    
     class Meta:
         model = models.Shopper
         fields = '__all__'
@@ -84,43 +71,44 @@ class ShopperSerializer(ModelSerializer):
                 return nickname
 
     def create(self, validated_data):
-        user_data = validated_data.pop('user')
-        user = models.User.objects.create_user(**user_data)
-        
-        if 'nickname' not in validated_data.keys():
-            validated_data['nickname'] = self.__get_random_nickname()
-        shopper = models.Shopper.objects.create(user=user, **validated_data)
+        validated_data['nickname'] = self.__get_random_nickname()
+        validated_data['password'] = make_password(validated_data['password'])
+        shopper = models.Shopper.objects.create(**validated_data)
         
         return shopper
 
     def update(self, instance, validated_data):
-        user = instance.user
-        user_data = validated_data.pop('user')
-
-        for key, value in user_data.items():            
-            setattr(user, key, value)
-        user.save()
-
-        for key ,value in validated_data.items():
+        for key, value in validated_data.items():            
             setattr(instance, key, value)
+        
         instance.save()
         
         return instance
 
 
 class WholesalerSerializer(ModelSerializer):
-    user = UserSerializer()
+    username = RegexField(r'^[a-zA-Z0-9]+$', min_length=4, max_length=30, validators=[UniqueValidator(queryset=models.User.objects.all())])
+    password = RegexField(r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[!-~]+$', max_length=128, min_length=10)
+    phone = RegexField(r'^01[0|1|6|7|8|9][0-9]{7,8}$')
+    company_registration_number = CharField(max_length=12, validators=[UniqueValidator(queryset=models.Wholesaler.objects.all())])
 
     class Meta:
         model = models.Wholesaler
         fields = '__all__'
 
     def create(self, validated_data):
-        user_data = validated_data.pop('user')
-        user = models.User.objects.create_user(**user_data)
-        wholesaler = models.Wholesaler.objects.create(user=user, **validated_data)
-        
+        validated_data['password'] = make_password(validated_data['password'])
+        wholesaler = models.Wholesaler.objects.create(**validated_data)
+
         return wholesaler
+
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():            
+            setattr(instance, key, value)
+        
+        instance.save()
+        
+        return instance
 
 
 class UserPasswordSerializer(Serializer):
