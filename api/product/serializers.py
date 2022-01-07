8b58ef01ff60ed8dbcd97a84737c2ea731735305
sjMOK <mokseungju@naver.com>
2022-01-07 17:00:53 +0900

    product.views 오타 수정, product.serializers 필드 이름 수정, common.views 에러 메세지 수정

--- api/product/serializers.py
+++ api/product/serializers.py
@@ -1,5 +1,6 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import *
 from . import models
+
 
 class CategorySerializer(ModelSerializer):
     class Meta:
@@ -14,6 +15,7 @@ class SubCategorySerializer(ModelSerializer):
 
 
 class ProductSerializer(ModelSerializer):
+    subcategory_id = PrimaryKeyRelatedField(queryset=models.SubCategory.objects.all())
     class Meta:
         model = models.Product
-        fields = '__all__'
+        exclude = ['subcategory']
