fec7c02ab20a45842b273dccabbe54b1e2be412e
sjMOK <mokseungju@naver.com>
2022-02-17 20:17:15 +0900

    product.models Model 추가(상품 등록 반영: SubCategorySize, ProductColor, ProductColorImages, Style, LaundryInformation, ProductLaundryInformation, Material, ProductMaterial)

--- api/product/models.py
+++ api/product/models.py
@@ -21,12 +21,24 @@ class SubCategory(Model):
     id = AutoField(primary_key=True)
     main_category = ForeignKey('MainCategory', DO_NOTHING)
     name = CharField(max_length=20)
+    sizes = ManyToManyField('Size', through='SubCategorySize')
 
     class Meta:
         db_table = 'sub_category'
 
     def __str__(self):
         return self.name
+
+
+class SubCategorySize(Model):
+    id = AutoField(primary_key=True)
+    sub_category = ForeignKey('SubCategory', DO_NOTHING)
+    size = ForeignKey('Size', DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'sub_category_size'
+        unique_together = (('sub_category', 'size'),)
 
 
 class Product(Model):
@@ -38,9 +50,12 @@ class Product(Model):
     price = IntegerField()
     wholesaler = ForeignKey('user.Wholesaler', DO_NOTHING)
     on_sale = BooleanField(default=True)
+    style = ForeignKey('Style', DO_NOTHING)
     tags = ManyToManyField('Tag', through='ProductTag')
-
+    laundry_informations = ManyToManyField('LaundryInformation', through='ProductLaundryInformation')
+    
     class Meta:
+        managed = False
         db_table = 'product'
 
     def __str__(self):
@@ -92,6 +107,30 @@ class Color(Model):
         return self.name
 
 
+class ProductColor(Model):
+    id = AutoField(primary_key=True)
+    product = ForeignKey('Product', DO_NOTHING)
+    color = ForeignKey('Color', DO_NOTHING)
+    display_color_name = CharField(max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'product_color'
+        unique_together = (('product', 'display_color_name'),)
+
+
+class ProductColorImages(Model):
+    id = AutoField(primary_key=True)
+    product_color = ForeignKey('ProductColor', DO_NOTHING)
+    url = CharField(max_length=200)
+    sequence = IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'product_color_images'
+        unique_together = (('product_color', 'sequence'), ('product_color', 'url'),)
+
+
 class Size(Model):
     id = AutoField(primary_key=True)
     name = CharField(unique=True, max_length=10)
@@ -111,8 +150,11 @@ class Option(Model):
     color = ForeignKey('Color', DO_NOTHING)
     display_color_name = CharField(max_length=20)
     price_difference = IntegerField(default=0)
+    product_color = ForeignKey('ProductColor', DO_NOTHING)
+    display_size_name = CharField(max_length=20)
 
     class Meta:
+        managed = False
         db_table = 'option'
 
     def save(self, *args, **kwargs):
@@ -127,3 +169,53 @@ class Keyword(Model):
 
     class Meta:
         db_table = 'keyword'
+
+
+class Style(Model):
+    id = AutoField(primary_key=True)
+    name = CharField(unique=True, max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'style'
+
+
+class LaundryInformation(Model):
+    id = AutoField(primary_key=True)
+    name = CharField(unique=True, max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'laundry_information'
+
+
+class ProductLaundryInformation(Model):
+    id = AutoField(primary_key=True)
+    product = ForeignKey('Product', DO_NOTHING)
+    laundry_information = ForeignKey('LaundryInformation', DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'product_laundry_information'
+        unique_together = (('product', 'laundry_information'),)
+
+
+class Material(Model):
+    id = AutoField(primary_key=True)
+    name = CharField(unique=True, max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'material'
+
+
+class ProductMaterial(Model):
+    id = AutoField(primary_key=True)
+    product = ForeignKey('Product', DO_NOTHING)
+    material = CharField(max_length=20)
+    mixing_rate = IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'product_material'
+        unique_together = (('product', 'material'),)
