a716a35714dea6c561ac23bc1748f118db3a2eb3
sjMOK <mokseungju@naver.com>
2022-02-21 20:00:06 +0900

    SerializerMixin 파일 위치 변경(common.mixins.py -> common.serializers.py), SerializerMixin 테스트 코드 추가

---
+++ api/common/serializers.py
@@ -0,0 +1,55 @@
+from rest_framework.exceptions import APIException
+from rest_framework.serializers import Serializer, ModelSerializer
+
+
+class SerializerMixin:
+    def __init__(self, *args, **kwargs):
+        if 'allow_fields' in kwargs and 'exclude_fields' in kwargs:
+            raise APIException('allow and exclude are incompatible.')
+
+        allow_fields = kwargs.pop('allow_fields', None)
+        exclude_fields = kwargs.pop('exclude_fields', None)
+
+        super().__init__(*args, **kwargs)
+        self.drop_fields(allow_fields, exclude_fields)
+
+    def drop_fields(self, allow_fields=None, exclude_fields=None):
+        if allow_fields is not None:
+            if not (isinstance(allow_fields, tuple) or isinstance(allow_fields, list)):
+                raise APIException('allow_fields must be tuple or list instance.')
+            self.remain_allow_fields(allow_fields)
+        elif exclude_fields is not None:
+            if not (isinstance(exclude_fields, tuple) or isinstance(exclude_fields, list)):
+                raise APIException('exclude_fields must be tuple or list instance.')
+            self.exclude_fields(exclude_fields)
+            
+
+    def remain_allow_fields(self, allow_fields):
+        allow_fields = set(allow_fields)
+        existing_fields = set(self.fields)
+
+        for field in allow_fields:
+            if field not in existing_fields:
+                raise APIException('allow_fields <{0}> not in serializer.fields.'.format(field))
+
+        fields = set(self.fields) - set(allow_fields)
+        for field in fields:
+            self.fields.pop(field)
+
+    def exclude_fields(self, exclude_fields):
+        exclude_fields = set(exclude_fields)
+        existing_fields = set(self.fields)
+
+        for field in exclude_fields:
+            if field not in existing_fields:
+                raise APIException('exclude_fields <{0}> not in serializer.fields.'.format(field))
+
+            self.fields.pop(field)
+
+
+class DynamicFieldSerializer(SerializerMixin, Serializer):
+    pass
+
+
+class DynamicFieldModelSerializer(SerializerMixin, ModelSerializer):
+    pass
--- api/common/test_mixins.py
+++ api/common/test_mixins.py
@@ -1,22 +1,23 @@
 from django.test import tag
 
 from rest_framework.test import APISimpleTestCase
-from rest_framework import serializers
+from rest_framework.serializers import Serializer, CharField, IntegerField
 from rest_framework.exceptions import APIException
 
-from .mixins import SerializerMixin
+from .serializers import SerializerMixin
 
 
-@tag('mixin')
 class SerializerMixinTestCase(APISimpleTestCase):
-    class DummySerializer(serializers.Serializer):
-        name = serializers.CharField(max_length=20)
-        age = serializers.IntegerField()
-        phone = serializers.CharField(max_length=20)
-        address = serializers.CharField(max_length=50)
+    class DummySerializer(Serializer):
+        name = CharField(max_length=20)
+        age = IntegerField()
+        phone = CharField(max_length=20)
+        address = CharField(max_length=50)
+
 
     class DummyTestSerializer(SerializerMixin, DummySerializer):
         pass
+
 
     def setUp(self):
         self.serializer_class = self.DummySerializer
@@ -48,22 +49,42 @@ class SerializerMixinTestCase(APISimpleTestCase):
             allow_fields=self.allow_fields, exclude_fields=self.exclude_fields
         )
 
-    def test_allow_fields_must_be_tuple(self):
-        allow_fields = list(self.allow_fields)
+    def test_allow_fields_must_be_tuple_or_list_by_string(self):
+        allow_fields = ('id')
 
         self.assertRaisesRegex(
             APIException,
-            r'^allow_fields must be tuple instance$',
+            r'^allow_fields must be tuple or list instance.$',
             self.test_serializer_class,
             allow_fields=allow_fields
         )
 
-    def test_exclude_fields_must_be_tuple(self):
-        exclude_fields = list(self.exclude_fields)
+    def test_allow_fields_must_be_tuple_or_list_by_set(self):
+        allow_fields = set(self.allow_fields)
 
         self.assertRaisesRegex(
             APIException,
-            r'^exclude_fields must be tuple instance$',
+            r'^allow_fields must be tuple or list instance.$',
+            self.test_serializer_class,
+            allow_fields=allow_fields
+        )
+
+    def test_exclude_fields_must_be_tuple_by_string(self):
+        exclude_fields = ('id')
+
+        self.assertRaisesRegex(
+            APIException,
+            r'^exclude_fields must be tuple or list instance.$',
+            self.test_serializer_class,
+            exclude_fields=exclude_fields
+        )
+
+    def test_exclude_fields_must_be_tuple(self):
+        exclude_fields = set(self.exclude_fields)
+
+        self.assertRaisesRegex(
+            APIException,
+            r'^exclude_fields must be tuple or list instance.$',
             self.test_serializer_class,
             exclude_fields=exclude_fields
         )
@@ -72,7 +93,7 @@ class SerializerMixinTestCase(APISimpleTestCase):
         self.allow_fields += ('gender',)
         self.assertRaisesRegex(
             APIException,
-            r'not in serializer.fields$',
+            r'not in serializer.fields.$',
             self.test_serializer_class,
             allow_fields=self.allow_fields
         )
@@ -81,7 +102,7 @@ class SerializerMixinTestCase(APISimpleTestCase):
         self.exclude_fields += ('gender',)
         self.assertRaisesRegex(
             APIException,
-            r'not in serializer.fields$',
+            r'not in serializer.fields.$',
             self.test_serializer_class,
             exclude_fields=self.exclude_fields
         )
