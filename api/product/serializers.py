4b20cb2e310641768324d5a72711eea90bc15fbf
sjMOK <mokseungju@naver.com>
2022-03-01 16:55:29 +0900

    ProductSerializer, ProductWriteSerializer 분리 + field 속성(require, allow_empty 등) 획일화

--- api/product/serializers.py
+++ api/product/serializers.py
@@ -3,7 +3,7 @@ from rest_framework.serializers import (
     PrimaryKeyRelatedField, URLField, DateTimeField, BooleanField,
 )
 from rest_framework.validators import UniqueValidator
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError, APIException
 
 from common.utils import DEFAULT_IMAGE_URL, BASE_IMAGE_URL
 from common.serializers import DynamicFieldsSerializer, convert_primary_key_related_field_to_serializer
@@ -17,14 +17,14 @@ from .models import (
 
 class SubCategorySerializer(Serializer):
     id = IntegerField(read_only=True)
-    name = CharField(max_length=20)
+    name = CharField(read_only=True, max_length=20)
 
 
 class MainCategorySerializer(DynamicFieldsSerializer):
     id = IntegerField(read_only=True)
-    name = CharField(max_length=20, validators=[UniqueValidator(queryset=MainCategory.objects.all())])
-    image_url = ImageField(max_length=200)
-    sub_category = SubCategorySerializer(many=True, source='sub_categories')
+    name = CharField(read_only=True, max_length=20, validators=[UniqueValidator(queryset=MainCategory.objects.all())])
+    image_url = ImageField(read_only=True, max_length=200)
+    sub_category = SubCategorySerializer(read_only=True, many=True, source='sub_categories')
 
 
 class ColorSerializer(ModelSerializer):
@@ -39,10 +39,8 @@ class SizeSerializer(ModelSerializer):
         fields = '__all__'
 
 
-class TagSerializer(ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = ['name']
+class TagSerializer(Serializer):
+    name = CharField(read_only=True, max_length=20)
 
 
 class ProductImagesListSerializer(ListSerializer):
@@ -101,7 +99,7 @@ class ProductColorImagesListSerializer(ListSerializer):
 
 
 class ProductColorImagesSerializer(Serializer):
-    image_url = CharField(max_length=200, required=True)
+    image_url = CharField(max_length=200)
     sequence = IntegerField(read_only=True, max_value=5, min_value=1)
     
     class Meta:
@@ -120,13 +118,13 @@ class ProductColorImagesSerializer(Serializer):
 
 class LaundryInformationSerializer(Serializer):
     id = IntegerField(read_only=True)
-    name = CharField(max_length=20)
+    name = CharField(read_only=True, max_length=20)
 
 
 class ProductAdditionalInformationSerializer(Serializer):
-    thickness = PrimaryKeyRelatedField(queryset=Thickness.objects.all())
-    see_through = PrimaryKeyRelatedField(queryset=SeeThrough.objects.all())
-    flexibility = PrimaryKeyRelatedField(queryset=Flexibility.objects.all())
+    thickness = PrimaryKeyRelatedField(write_only=True, queryset=Thickness.objects.all())
+    see_through = PrimaryKeyRelatedField(write_only=True, queryset=SeeThrough.objects.all())
+    flexibility = PrimaryKeyRelatedField(write_only=True, queryset=Flexibility.objects.all())
     lining = BooleanField()
 
     def to_representation(self, instance):
@@ -153,7 +151,8 @@ class ProductMaterialListSerializer(ListSerializer):
 
 
 class ProductMaterialSerializer(Serializer):
-    material = CharField(max_length=20, required=True)
+    id = IntegerField(read_only=True)
+    material = CharField(max_length=20)
     mixing_rate = IntegerField(max_value=100, min_value=1)
 
     class Meta:
@@ -173,7 +172,7 @@ class OptionSerializer(Serializer):
     id = IntegerField(read_only=True)
     display_size_name = CharField(allow_null=True, max_length=20)
     price_difference = IntegerField(max_value=100000, min_value=0, required=False)
-    size = PrimaryKeyRelatedField(allow_null=True, queryset=Size.objects.all())
+    size = PrimaryKeyRelatedField(write_only=True, allow_null=True, queryset=Size.objects.all())
 
     class Meta:
         list_serializer_class = OptionListSerializer
@@ -200,10 +199,10 @@ class ProductColorListSerializer(ListSerializer):
 
 
 class ProductColorSerializer(Serializer):
-    display_color_name = CharField(max_length=20, required=False)
+    display_color_name = CharField(allow_null=True, max_length=20)
     color = PrimaryKeyRelatedField(queryset=Color.objects.all())
-    options = OptionSerializer(allow_empty=False, many=True, required=True)
-    images = ProductColorImagesSerializer(allow_empty=False, many=True, required=True)
+    options = OptionSerializer(allow_empty=False, many=True)
+    images = ProductColorImagesSerializer(allow_empty=False, many=True)
 
     class Meta:
         list_serializer_class = ProductColorListSerializer
@@ -217,42 +216,56 @@ class ProductColorSerializer(Serializer):
 
 
 class AgeSerializer(Serializer):
-    id = IntegerField(read_only=True)
     name = CharField(max_length=10, read_only=True)
 
 
 class StyleSerializer(Serializer):
-    id = IntegerField(read_only=True)
     name = CharField(max_length=20, read_only=True)
 
 
-class ProductCreateSerializer(DynamicFieldsSerializer):
+class ProductSerializer(DynamicFieldsSerializer):
     id = IntegerField(read_only=True)
+    name = CharField(max_length=60)
+    price = IntegerField(max_value=1000000, min_value=0)
     main_category = MainCategorySerializer(read_only=True, source='sub_category.main_category', allow_fields=('id', 'name'))
-    sub_category = PrimaryKeyRelatedField(queryset=SubCategory.objects.all(), required=True)
-    name = CharField(max_length=60, required=True)
-    price = IntegerField(max_value=1000000, min_value=0, required=True)
-    style = PrimaryKeyRelatedField(queryset=Style.objects.all(), required=True)
-    age = PrimaryKeyRelatedField(queryset=Age.objects.all(), required=True)
-    tags = PrimaryKeyRelatedField(allow_empty=True, many=True, queryset=Tag.objects.all(), required=True)
-    laundry_informations = PrimaryKeyRelatedField(allow_empty=True, many=True, queryset=LaundryInformation.objects.all(), required=True)
-    product_additional_information = ProductAdditionalInformationSerializer(allow_null=False)
-    materials = ProductMaterialSerializer(allow_empty=False, many=True, required=True)
-    colors = ProductColorSerializer(allow_empty=False, many=True, required=True)
-    images = ProductImagesSerializer(allow_empty=False, many=True, required=True, source='related_images')
+    sub_category = SubCategorySerializer(read_only=True)
+    style = StyleSerializer(read_only=True)
+    age = AgeSerializer(read_only=True)
+    tags = TagSerializer(read_only=True, many=True)
+    laundry_informations = LaundryInformationSerializer(read_only=True, many=True)
+    product_additional_information = ProductAdditionalInformationSerializer()
+    materials = ProductMaterialSerializer(allow_empty=False, many=True)
+    colors = ProductColorSerializer(allow_empty=False, many=True)
+    images = ProductImagesSerializer(allow_empty=False, many=True, source='related_images')
 
-    primary_key_related_field_serializer_mapping = [
-        {'field': 'sub_category', 'serializer_class': SubCategorySerializer, 'many': False},
-        {'field': 'style', 'serializer_class': StyleSerializer, 'many': False},
-        {'field': 'age', 'serializer_class': AgeSerializer, 'many': False},
-        {'field': 'tags', 'serializer_class': TagSerializer, 'many': True},
-        {'field': 'laundry_informations', 'serializer_class': LaundryInformationSerializer, 'many': True},
-    ]
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+
+        if self.context['detail']:
+            ret = self.to_representation_retrieve(ret, instance)
+        else:
+            ret['main_image'] = (BASE_IMAGE_URL + instance.related_images[0].image_url) if instance.related_images else DEFAULT_IMAGE_URL
+
+        return ret
+
+    def to_representation_retrieve(self, ret, instance):
+        if not instance.related_images:
+            ret['images'] = [DEFAULT_IMAGE_URL]
+
+        return ret
+
+
+class ProductWriteSerializer(ProductSerializer):
+    sub_category = PrimaryKeyRelatedField(write_only=True, queryset=SubCategory.objects.all())
+    style = PrimaryKeyRelatedField(write_only=True, queryset=Style.objects.all())
+    age = PrimaryKeyRelatedField(write_only=True, queryset=Age.objects.all())
+    tags = PrimaryKeyRelatedField(write_only=True, many=True, queryset=Tag.objects.all())
+    laundry_informations = PrimaryKeyRelatedField(write_only=True, many=True, queryset=LaundryInformation.objects.all())
 
     def validate(self, attrs):
         price = attrs.get('price')
-
-        product_colors = attrs.get('colors')
+        
+        product_colors = attrs.get('colors', list())
         for product_color in product_colors:
             options = product_color.get('options')
             validate_price_difference(price, options)
@@ -262,7 +275,7 @@ class ProductCreateSerializer(DynamicFieldsSerializer):
     def create(self, validated_data):
         laundry_informations = validated_data.pop('laundry_informations', list())
         tags = validated_data.pop('tags', list())
-        product_additional_information = validated_data.pop('product_additional_information', None)
+        product_additional_information = validated_data.pop('product_additional_information')
         materials = validated_data.pop('materials')
         colors = validated_data.pop('colors')
         images = validated_data.pop('related_images')
@@ -276,8 +289,7 @@ class ProductCreateSerializer(DynamicFieldsSerializer):
         
         product.tags.add(*tags)
 
-        if product_additional_information is not None:
-            ProductAdditionalInformation.objects.create(product=product, **product_additional_information)
+        ProductAdditionalInformation.objects.create(product=product, **product_additional_information)
 
         ProductMaterial.objects.bulk_create(
             [ProductMaterial(product=product, **material_data) for material_data in materials]
@@ -310,79 +322,6 @@ class ProductCreateSerializer(DynamicFieldsSerializer):
     def update(self, instance, validated_data):
         return super().update(instance, validated_data)
 
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-
-        if self.context['detail']:
-            ret = self.to_representation_retrieve(ret, instance)
-        else:
-            ret['main_image'] = (BASE_IMAGE_URL + instance.related_images[0].image_url) if instance.related_images else DEFAULT_IMAGE_URL
-
-        return ret
-
-    def to_representation_retrieve(self, ret, instance):
-        for mapping in self.primary_key_related_field_serializer_mapping:
-            ret[mapping.get('field')] = convert_primary_key_related_field_to_serializer(instance=instance, **mapping)
-        
-        if not instance.related_images:
-            ret['images'] = [DEFAULT_IMAGE_URL]
-
-        return ret
-
-
-class ProductSerializer(DynamicFieldsSerializer):
-    id = IntegerField(read_only=True)
-    name = CharField(max_length=60)
-    code = CharField(max_length=12, read_only=True)
-    created = DateTimeField(read_only=True)
-    price = IntegerField(max_value=2147483647, min_value=0)
-    sub_category = PrimaryKeyRelatedField(write_only=True, queryset=SubCategory.objects.all())
-    options = OptionSerializer(many=True, source='related_options')
-    images = ProductImagesSerializer(many=True, source='related_images')
-    tags = TagSerializer(many=True, source='related_tags')
-    on_sale = BooleanField(required=False)
-
-    def to_representation_retrieve(self, ret, instance):
-        ret['sub_category'] = SubCategorySerializer(instance.sub_category).data
-        ret['main_category'] = MainCategorySerializer(instance.sub_category.main_category).data
-        ret['tags'] = [tag['name'] for tag in ret['tags']]
-
-        related_images = ret.get('images', None)
-        if related_images is not None:
-            for image in related_images:
-                image['iamge_url'] = BASE_IMAGE_URL + image['image_url']
-        else:
-            ret['images'] = [DEFAULT_IMAGE_URL]
-
-        return ret
-
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-        
-        if self.context['detail']:
-            ret = self.to_representation_retrieve(ret, instance)
-        else:
-            ret['main_image'] = (BASE_IMAGE_URL + instance.related_images[0].image_url) if instance.related_images else DEFAULT_IMAGE_URL
-
-        return ret
-
-    def create(self, validated_data):
-        related_options = validated_data.pop('related_options')
-        related_images = validated_data.pop('related_images')
-
-        product = Product.objects.create(wholesaler=self.context['wholesaler'], **validated_data)
-
-        options = [Option(product=product, **option_data) for option_data in related_options ]
-        Option.objects.bulk_create(options)
-
-        images = [ProductImages(product=product, **image_data) for image_data in related_images]
-        ProductImages.objects.bulk_create(images)
-
-        return product
-    
-    def update(self, instance, validated_data):
-        return super().update(instance, validated_data)
-
 
 class ImageSerializer(Serializer):
     image = ImageField(max_length=200, validators=[validate_file_size])
@@ -393,21 +332,13 @@ class MaterialSerializer(Serializer):
     name = CharField(max_length=20, read_only=True)
 
 
-
-
-
-
-
 class ThicknessSerializer(Serializer):
-    id = IntegerField(read_only=True)
     name = CharField(max_length=10, read_only=True)
 
 
 class SeeThroughSerializer(Serializer):
-    id = IntegerField(read_only=True)
     name = CharField(max_length=10, read_only=True)
 
 
 class FlexibilitySerializer(Serializer):
-    id = IntegerField(read_only=True)
     name = CharField(max_length=10, read_only=True)
