7f2d0f8a0ceb2daeba6bb38cb69267c29f88a4d6
sjMOK <mokseungju@naver.com>
2022-03-23 16:48:24 +0900

    product.theme allow_null=True

--- api/product/serializers.py
+++ api/product/serializers.py
@@ -512,7 +512,7 @@ class ProductWriteSerializer(ProductSerializer):
     thickness = PrimaryKeyRelatedField(queryset=Thickness.objects.all())
     see_through = PrimaryKeyRelatedField( queryset=SeeThrough.objects.all())
     flexibility = PrimaryKeyRelatedField(queryset=Flexibility.objects.all())
-    theme = PrimaryKeyRelatedField(queryset=Theme.objects.all())
+    theme = PrimaryKeyRelatedField(queryset=Theme.objects.all(), allow_null=True)
 
     price_difference_upper_limit_ratio = 0.2
 
