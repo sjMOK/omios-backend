8fcde5c0cd26ff0fc9cb24ff9cecb3d9b7b003ee
sjMOK <mokseungju@naver.com>
2022-03-31 17:08:58 +0900

    setting test db 수정

--- api/config/settings.py
+++ api/config/settings.py
@@ -89,9 +89,6 @@ DATABASES = {
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
         'PORT': os.environ.get("DB_PORT"),
-        'TEST': {
-            'NAME': 'mokseungju_test_database',
-        }
     }
 }
 
