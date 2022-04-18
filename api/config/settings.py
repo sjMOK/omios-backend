627c63c9437cd95ed46d0b16aebc250fca61a0dd
sjMOK <mokseungju@naver.com>
2022-04-18 13:43:34 +0900

    change dir common/ to django app

--- api/config/settings.py
+++ api/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'django_filters',
     'drf_yasg',
+    'common.apps.CommonConfig',
     'user.apps.UserConfig',
     'product.apps.ProductConfig',
 ]
