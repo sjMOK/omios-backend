c51f903795de33265f7673f58341c0094e129482
KangDaeJin <rkdeowls2580@naver.com>
2022-05-03 17:29:29 +0900

    Remove BadRequestError Exception Implement NotExcutableValidationError Exception

--- api/common/exceptions.py
+++ api/common/exceptions.py
@@ -1,6 +1,5 @@
 from rest_framework.exceptions import APIException
-from rest_framework.status import HTTP_400_BAD_REQUEST
 
 
-class BadRequestError(APIException):
-    status_code = HTTP_400_BAD_REQUEST
+class NotExcutableValidationError(APIException):
+    default_detail = 'This serializer cannot validate.'
