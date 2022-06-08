215feaad13c7a4dbfe821e4eed43e10e82e978d6
sjMOK <mokseungju@naver.com>
2022-06-08 14:24:42 +0900

    CouponSerializer 구현

--- api/coupon/serializers.py
+++ api/coupon/serializers.py
@@ -1,6 +1,9 @@
+from datetime import date
+
 from rest_framework.serializers import (
-    ModelSerializer
+    ModelSerializer,
 )
+from rest_framework.exceptions import ValidationError
 
 from .models import Coupon
 
@@ -8,11 +11,60 @@ from .models import Coupon
 class CouponSerializer(ModelSerializer):
     class Meta:
         model = Coupon
-        fields = '__all__'
+        exclude = ['products', 'sub_categories']
         extra_kwargs = {
             'discount_rate': {'min_value': 0, 'max_value': 100},
-            'discount_price': {'min_value': 1, 'max_value': 1000000},
-            'minimum_order_price': {'min_value': 0},
-            'maximum_discount_price': {'min_value': 0},
+            'discount_price': {'min_value': 0, 'max_value': 1000000},
+            'minimum_order_price': {'min_value': 0, 'max_value': 1000000},
+            'maximum_discount_price': {'min_value': 0, 'max_value': 1000000},
             'available_period': {'min_value': 0},
         }
+
+    def to_representation(self, instance):
+        result = super().to_representation(instance)
+
+        if result['id'] in self.context.get('owned_coupon_id_list', []):
+            result['coupon_owned'] = True
+        else:
+            result['coupon_owned'] = False
+
+        return result
+
+    def validate_end_date(self, value):
+        if value < date.today():
+            raise ValidationError('Please check the end_date of the coupon.')
+
+        return value
+
+    def validate(self, attrs):
+        discount_rate = attrs.get('discount_rate', None)
+        discount_price = attrs.get('discount_price', None)
+        self.__validate_discount_rate_and_discount_price_exclusive_or(discount_rate, discount_price)
+
+        start_date = attrs.get('start_date', None)
+        end_date = attrs.get('end_date', None)
+        available_period = attrs.get('available_period', None)
+        self.__validate_expiration_data(start_date, end_date, available_period)
+
+        return attrs
+
+    def __validate_discount_rate_and_discount_price_exclusive_or(self, discount_rate, discount_price):
+        if not (bool(discount_rate) ^ bool(discount_price)):
+            raise ValidationError('Only one of discount_rate or discount_price must exist.')
+
+    def __validate_expiration_data(self, start_date, end_date, available_period):
+        self.__validate_expiration_data_exclusive_or(start_date, end_date, available_period)
+        if available_period is None:
+            self.__validate_start_date_and_end_date(start_date, end_date)
+
+    def __validate_expiration_data_exclusive_or(self, start_date, end_date, available_period):
+        date_data = [data for data in [start_date, end_date] if data is not None]
+
+        if not (bool(date_data) ^ bool(available_period)):
+            raise ValidationError('Only one of date or available_period must exist.')
+
+    def __validate_start_date_and_end_date(self, start_date, end_date):
+        if not (start_date and end_date):
+            raise ValidationError('Both start_date and end_date must exist.')
+        elif start_date > end_date:
+            raise ValidationError('The start date must be before the end date.')
--- api/coupon/test/test_serializers.py
+++ api/coupon/test/test_serializers.py
@@ -1,1 +1,119 @@
+from datetime import date, timedelta
+
 from common.test.test_cases import SerializerTestCase
+from common.utils import datetime_to_iso
+from user.test.factories import ShopperFactory, ShopperCouponFactory
+from .factories import CouponFactory, CouponClassificationFactory
+from ..serializers import CouponSerializer
+
+class CouponSerializerTestCase(SerializerTestCase):
+    _serializer_class = CouponSerializer
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.__coupon_classification = CouponClassificationFactory()
+        cls.__coupon = CouponFactory(classification=cls.__coupon_classification)
+        cls._test_data = {
+            'name': 'super coupon',
+            'discount_price': 10000,
+            'start_date': date.today() - timedelta(weeks=1),
+            'end_date': date.today() + timedelta(weeks=1),
+            'is_auto_issue': False,
+            'classification': cls.__coupon_classification.id,
+        }
+
+    def test_model_instance_serialization(self):
+        self._test_model_instance_serialization(self.__coupon, {
+            'id': self.__coupon.id,
+            'name': self.__coupon.name,
+            'discount_rate': self.__coupon.discount_rate,
+            'discount_price': self.__coupon.discount_price,
+            'minimum_order_price': self.__coupon.minimum_order_price,
+            'maximum_discount_price': self.__coupon.maximum_discount_price,
+            'start_date': datetime_to_iso(self.__coupon.start_date),
+            'end_date': datetime_to_iso(self.__coupon.end_date),
+            'available_period': self.__coupon.available_period,
+            'is_auto_issue': self.__coupon.is_auto_issue,
+            'classification': self.__coupon.classification.id,
+            'coupon_owned': False,
+        })
+
+    def test_model_instance_serialization_coupon_owned_true(self):
+        shopper = ShopperFactory()
+        ShopperCouponFactory(shopper=shopper, coupon=self.__coupon)
+
+        owned_coupon_id_list = list(shopper.coupons.all().values_list('id', flat=True))
+        context = {'owned_coupon_id_list': owned_coupon_id_list}
+
+        self._test_model_instance_serialization(self.__coupon, {
+            'id': self.__coupon.id,
+            'name': self.__coupon.name,
+            'discount_rate': self.__coupon.discount_rate,
+            'discount_price': self.__coupon.discount_price,
+            'minimum_order_price': self.__coupon.minimum_order_price,
+            'maximum_discount_price': self.__coupon.maximum_discount_price,
+            'start_date': datetime_to_iso(self.__coupon.start_date),
+            'end_date': datetime_to_iso(self.__coupon.end_date),
+            'available_period': self.__coupon.available_period,
+            'is_auto_issue': self.__coupon.is_auto_issue,
+            'classification': self.__coupon.classification.id,
+            'coupon_owned': True,
+        }, context)
+
+    def test_raise_validation_error_discount_rate_and_discount_price_are_not_exclusive_or(self):
+        self._test_data['discount_rate'] = 10
+        self._test_serializer_raise_validation_error(
+            'Only one of discount_rate or discount_price must exist.',
+            data=self._test_data
+        )
+
+    def test_raise_validation_error_end_date_less_than_today(self):
+        self._test_data['end_date'] = date.today() - timedelta(days=1)
+        self._test_serializer_raise_validation_error(
+            'Please check the end_date of the coupon.',
+            data=self._test_data
+        )
+    
+    def test_raise_validation_error_start_date_greater_than_today(self):
+        self._test_data['start_date'] = self._test_data['end_date'] + timedelta(days=1)
+        self._test_serializer_raise_validation_error(
+            'The start date must be before the end date.',
+            data=self._test_data
+        )
+
+    def test_raise_validation_error_only_start_date(self):
+        self._test_data.pop('end_date')
+        self._test_serializer_raise_validation_error(
+            'Both start_date and end_date must exist.',
+            data=self._test_data
+        )
+
+    def test_raise_validation_error_only_end_date(self):
+        self._test_data.pop('start_date')
+        self._test_serializer_raise_validation_error(
+            'Both start_date and end_date must exist.',
+            data=self._test_data
+        )
+
+    def test_raise_validation_error_expiration_data_is_not_exclusive_or(self):
+        self._test_data['available_period'] = 14
+        self._test_serializer_raise_validation_error(
+            'Only one of date or available_period must exist.',
+            data=self._test_data
+        )
+    
+    def test_raise_validation_error_start_date_and_available_period_passed_together(self):
+        self._test_data['available_period'] = 14
+        self._test_data.pop('end_date')
+        self._test_serializer_raise_validation_error(
+            'Only one of date or available_period must exist.',
+            data=self._test_data
+        )
+
+    def test_raise_validation_error_end_date_and_available_period_passed_together(self):
+        self._test_data['available_period'] = 14
+        self._test_data.pop('start_date')
+        self._test_serializer_raise_validation_error(
+            'Only one of date or available_period must exist.',
+            data=self._test_data
+        )
