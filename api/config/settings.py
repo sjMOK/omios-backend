5b67fd15243ceac689a6ed816350e5f71f15f6e8
sjMOK <mokseungju@naver.com>
2024-07-30 20:15:55 +0900

    delete omios host name from allowed_hosts

--- api/config/settings.py
+++ api/config/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'omios']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'omios']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
